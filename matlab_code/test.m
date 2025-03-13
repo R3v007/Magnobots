d40147618c510bc2d4ede3cda51b4043caf1fbae
Rev <92115454+R3v007@users.noreply.github.com>
2025-03-13 13:43:39 +0000

    better comments

--- matlab_code/test.m
+++ matlab_code/test.m
@@ -3,24 +3,20 @@ clear;
 close all;
 
 % TODO
-% 5. convergence
-% 2. Animation?
-% 3. Hall Sensor Simulink!
-% 4. Control on Simulink??
-% 6. ENERGY OF THE WHOLE SYSTEM AND TRY TO FIND THE LEAST ENERGY POINT?
+% 1. forgot the system mechanics
 
 %% Constants and Other Definitions
 
 mu0 = 4*pi*1e-7; % Vacuum permeability
-d = 0.042; % Length of the dipole
-w = 0.008; % Dipole width
-thick = 0.01; % Dipole Thickness
+d = 0.020; % Length of the dipole
+w = 0.003; % Dipole width
+thick = 0.05; % Dipole Thickness
 M = 0.005; % Mass of the dipole
 I = (M*d*d)/12; % Moment of inertia calculation 
 n=3; %number of magnets
 
 %from datasheet
-Br= 1.2; % N42 value
+Br= 1.42; % N52 value
 m_datasheet = Br*(d*w*thick)/mu0;
 
 %% Variables and Matrices Setup
@@ -37,11 +33,13 @@ r=zeros(n,n,3); % Magnet Position vectors between 2 magent centres nxn
 m=zeros(n,2); % Dipole moment vectors
 B = zeros(n,n); % Magnet Field Vectors due to ther dipoles
 tau = zeros(n,1); % Torque vectors
+energy=zeros(n,1); % clculating energy for each dipole
 alpha = zeros(n,1); % Angular acc
+w=zeros(n,1);%angular velocity
 
-T = 2; % Total time of simulation
+T = 1000; % Total time of simulation
 ts = linspace(0, T, 200); % Time vector -> decrease time step to converge
-tracking=zeros(n,2,length(ts)+1);
+tracking=zeros(n,2,length(ts)+1); %tracking trajecotry of dipoles
 
 %% Initial values setup
 
@@ -52,11 +50,12 @@ for i=1:n
     pos(i,2)=input("Enter initial y position");
 end
 
-%% le for loop that killed me : only calcs. 
+%% magnetic field calculayions 
 % get data first, do figure later
 
 bmax=0;
 tau_max=0;
+energy_min=1;
 
 for t=1:length(ts)
     tracking(:,:,t)=pos(:,:);
@@ -71,6 +70,7 @@ for t=1:length(ts)
         disp([xp, yp]);
         m1_vec=[m(i,1),m(i,2),0]; %current dipole m to be used in torque calc
         net_tau =0;
+        net_energy =0;
        for j=1:n %inner loop to calculate net toruque and field
            if(j==i)
                continue;
@@ -87,6 +87,7 @@ for t=1:length(ts)
             denom = ( norm(r_vec))^5
             dot_pord = 3*dot(m2_vec,r_vec)*r_vec;
             other= m2_vec*(norm(r_vec))^2;
+            % force due to m2 on m1
             B_vec = k*(dot_pord-other)/denom
             
            
@@ -97,25 +98,39 @@ for t=1:length(ts)
             end
 
              %total torque from all fields from all magnets
+             % torque due to field by m2 on m1
             net_tau = net_tau + cross(m1_vec, B_vec);
+            net_energy = net_energy+dot(m1_vec, B_vec)
        end
        if (norm(net_tau)>tau_max)
-           tau_max=norm(net_tau);
+           tau_max=norm(net_tau); %for self tracking
        end
+       
         tau(i,1)=net_tau(3) % net toruq
+        energy(i,1)=net_energy; % total energy of the system
         alpha(i,1)=tau(i,1)/I % angular acc
+        w(i,1)=w(i,1)+alpha(i,1)*0.01; %angular vel for this time step
+        del_theta = w(i,1)*0.01; %change in angle for this time step
         rad = norm([xp, yp]-[0,0]) %distance from origin
-        del_theta = alpha(i,1)*rad%net angle rotated by
+        del_moved = del_theta*rad%net angle rotated by
         theta(i,1)=theta(i,1)+del_theta; %new angle for later
         
         rot_mat = [cos(del_theta), -sin(del_theta); 
-            sin(del_theta), cos(del_theta)];
+            sin(del_theta), cos(del_theta)]; % claculating the new position
         new = rot_mat*[xp; yp]
         pos(i,1)=new(1);
         pos(i,2)=new(2);
         tracking(i,:,t)=new;
     end
-    
+    sum_e=sum(energy);
+    if (sum_e<energy_min)
+        disp("new low energy reached");
+        disp(["timestep is: ", t]);
+        disp(tracking(:,:,t));
+        energy_min=sum_e
+        input("press enter to continue");
+     end
+
 end
 
 %% quick plotting
@@ -128,49 +143,16 @@ xlabel('X Position (m)');
 ylabel('Y Position (m)');
 
 
-x(:)=tracking(2, 1, :);
-y(:)=tracking(2, 2, :);
-plot(x,y , 'Color', rand(1, 3)); % Trajectories with random colors
+x2(:)=tracking(2, 1, :);
+y2(:)=tracking(2, 2, :);
 
-%% Magnetostatic Model
+x1(:)=tracking(1, 1, :);
+y1(:)=tracking(1, 2, :);
+x3(:)=tracking(3, 1, :);
+y3(:)=tracking(3, 2, :);
 
-frame=[3;4;[0.2; 0.2; -0.2; -0.2]; 0.2; -0.2; -0.2; 0.2];
-
-%creating the geometry
-magnet1=[3;4;[0.021; 0.021; -0.021; -0.021; 
-    0.004;   -0.004;  -0.004;  0.004]];
-
-magnet2=[3;4;[0.171; 0.171; 0.129; 0.129; 
-    0.004;-0.004; -0.004; 0.004]];
-
-gd=[frame, magnet1, magnet2];
-sf="frame+magnet1+magnet2";
-ns=char('frame','magnet1','magnet2');
-ns=ns';
-dl=decsg(gd, sf, ns);
-
-figure;
-pdegplot(dl, FaceLabels="on");
-%%
-model = femodel("AnalysisType","magnetostatic", "Geometry",dl);
-model.VacuumPermeability=1.256e-6;
-model.MaterialProperties(2)=materialProperties(RelativePermeability=1.05);
-model.MaterialProperties(3)=materialProperties(RelativePermeability=1.05);
-model.MaterialProperties(1)=materialProperties(RelativePermeability=1);
-Mag=1.3;
-dir1 = [1;0];
-dir2 = [1;0];
-model.FaceLoad(2) = faceLoad(Magnetization=Mag*dir1);
-model.FaceLoad(3) = faceLoad(Magnetization=Mag*dir2);
-
-model = generateMesh(model, Hface={[2 3], 0.001}, Hmax=0.01, Hgrad=2);
-pdemesh(model);
-%%
-R = solve(model);
-Bmag = sqrt(R.MagneticFluxDensity.Bx.^2 + R.MagneticFluxDensity.By.^2);
-Hmag = sqrt(R.MagneticField.Hx.^2 + R.MagneticField.Hy.^2);
-
-figure
-pdeplot(R.Mesh,XYData=Bmag, ...
-               FlowData=[R.MagneticFluxDensity.Bx ...
-                         R.MagneticFluxDensity.By])
+plot(x2,y2,'DisplayName','Magnet2'); % Trajectories with random colors
+hold on
+plot(x1, y1,'DisplayName','Magnet1');
+hold on
+plot(x3,y3,'DisplayName','Magnet3');
